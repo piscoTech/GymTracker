1e00043343151c393c6f8c4690f253e4355ef9ee
Marco Boschi <boschi95@gmail.com>
2016-11-14 13:13:56 +0100

    Initial Commit

---
+++ Gym Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gym Tracker
+//
+//  Created by Marco Boschi on 14/11/2016.
+//  Copyright © 2016 Marco Boschi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
